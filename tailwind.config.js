0d21e839dda006d7a20e0b52a0f623ea2eff488e
Harry McCarthy <d4m3on@outlook.com>
2021-04-03 16:06:03 +1300

    Add the header image and the title

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ['./src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: theme => ({
+        'background': {
+          default: "#F2F2F2"
+        },
+        'headerText': {
+          default: "#072942"
+        },
+        'text': {
+          default: "#052033",
+          dark: "#F2F2F2"
+        }
+      })
+    },
   },
   variants: {
     extend: {},
