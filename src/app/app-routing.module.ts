231dce6cce7fe0d26d9c51e8a7729a765d4f0927
Harry McCarthy <d4m3on@outlook.com>
2021-06-07 16:34:19 +1200

    Add the about page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {JoinComponent} from './join/join.component';
 import {ContactComponent} from './contact/contact.component';
+import {AboutComponent} from './about/about.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'join', component: JoinComponent},
-  {path: 'contact', component: ContactComponent}
+  {path: 'contact', component: ContactComponent},
+  {path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
